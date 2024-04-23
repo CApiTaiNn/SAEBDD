69f942af3157f395a0f4146f61829c38e136f0c0
capitainn <morgantsloan@gmail.com>
2024-04-23 10:08:40 +0200

    first

---
+++ statistiques_colleges_db_create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Region{
+    code_region     VARCHAR(2),
+    libelle_region  VARCHAR(50)
+}
+
+CREATE TABLE Departement{
+    code_region     VARCHAR(2),
+    libelle_region  VARCHAR(50)
+}
+
