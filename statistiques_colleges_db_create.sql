655cf0ab041ec12aa499dbadad85150abc54c6ef
capitainn <morgantsloan@gmail.com>
2024-04-23 10:13:18 +0200

    ajout de comunne

--- statistiques_colleges_db_create.sql
+++ statistiques_colleges_db_create.sql
@@ -1,10 +1,17 @@
-CREATE TABLE Region{
-    code_region     VARCHAR(2),
-    libelle_region  VARCHAR(50)
-}
+CREATE TABLE Region(
+    code_region         VARCHAR(3),
+    libelle_region      VARCHAR(50) UNIQUE
+);
 
-CREATE TABLE Departement{
-    code_region     VARCHAR(2),
-    libelle_region  VARCHAR(50)
-}
+CREATE TABLE Departement(
+    code_departement    VARCHAR(3),
+    nom_departement     VARCHAR(50) UNIQUE
+);
+
+
+CREATE TABLE Commune(
+    code_insee_de_la_commune   VARCHAR(3),
+    nom_departement     VARCHAR(50) UNIQUE
+);
+
 
