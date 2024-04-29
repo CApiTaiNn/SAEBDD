0b5eaa8dad9d300284436e19b730e596861d4c4e
capitainn <morgantsloan@gmail.com>
2024-04-29 22:26:47 +0200

    ajout des 1ere table

--- statistiques_colleges_db_create.sql
+++ statistiques_colleges_db_create.sql
@@ -1,17 +1,23 @@
 CREATE TABLE Region(
-    code_region         VARCHAR(3),
-    libelle_region      VARCHAR(50) UNIQUE
+    code_region                VARCHAR(3),
+    libelle_region             VARCHAR(50) UNIQUE
 );
 
 CREATE TABLE Departement(
-    code_departement    VARCHAR(3),
-    nom_departement     VARCHAR(50) UNIQUE
+    code_departement           VARCHAR(3),
+    nom_departement            VARCHAR(50) UNIQUE
 );
 
 
 CREATE TABLE Commune(
     code_insee_de_la_commune   VARCHAR(3),
-    nom_departement     VARCHAR(50) UNIQUE
+    nom_dep_la_commune         VARCHAR(50) UNIQUE
+);
+
+
+CREATE TABLE Quartier_prioritaire(
+    code_quartier_prioritaire  VARCHAR(3),
+    nom_quartier_prioritaire   VARCHAR(50) UNIQUE
 );
 
 
