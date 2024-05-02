f54aebdbefea8e15127164bf9674b1d759f07f05
capitainn <morgantsloan@gmail.com>
2024-05-02 15:09:20 +0200

    table fini

--- statistiques_colleges_db_create.sql
+++ statistiques_colleges_db_create.sql
@@ -3,13 +3,27 @@ create schema "sae_bdd";
 set schema 'sae_bdd'; 
 
 --table creer :
---region
---departement
---commune
---quartier prioritaire
---Type
---etablissement
 
+--region  
+    --contrainte faite
+--departement
+    --contrainte faite
+--commune
+    --contrainte faite
+--etablissement
+    --je suis pas sur de celle la ell est pas faite
+--quartier prioritaire
+
+--etablissement
+    --contrainte faite
+--Type
+
+--carac_tout_etablissement
+--annee
+--carac_college
+--classe
+--carac_tout_etablissement
+--a_proximimté
 
 
 
@@ -19,17 +33,14 @@ CREATE TABLE _region(
 );
 
 
---alter table fait 
-
 CREATE TABLE _departement(
     code_departement           VARCHAR(3) PRIMARY KEY,
     nom_departement            VARCHAR(50) UNIQUE,
     code_region                VARCHAR(3)
 );
 
-ALTER TABLE 
 
---alter table
+
 CREATE TABLE _commune(
     code_insee_de_la_commune   VARCHAR(3) PRIMARY KEY,
     nom_dep_la_commune         VARCHAR(50) UNIQUE,
@@ -42,78 +53,94 @@ CREATE TABLE _quartier_prioritaire(
     nom_quartier_prioritaire   VARCHAR(50) UNIQUE
 );
 
+
 CREATE TABLE _type(
-    code_nat VARCHAR(50)PRIMARY KEY,
-    libelle_nature VARCHAR(50)
+    code_nature                VARCHAR(50)PRIMARY KEY,
+    libelle_nature             VARCHAR(50)
 );
--- alter table
+
+
+CREATE TABLE _academie(
+    code_academie              INTEGER PRIMARY KEY,
+    lib_academie               VARCHAR(50) UNIQUE
+);
+
 CREATE TABLE _etablissement(
-    uai integer PRIMARY KEY,
-    nom_etablissement VARCHAR(50),
-    secteur VARCHAR(50),
-    code_postal VARCHAR(50),
-    lattitude real, 
-    longitude real,
-    code_insee_de_la_commune VARCHAR(50),
-    code_academie integer,
-    code_nat VARCHAR(50)
+    uai                        integer PRIMARY KEY,
+    nom_etablissement          VARCHAR(50),
+    secteur                    VARCHAR(50),
+    code_postal                VARCHAR(50),
+    lattitude                  real, 
+    longitude                  real,
+    code_academie              integer,
+    code_nat                   VARCHAR(50),
+    code_commune               VARCHAR(3),
+    code_nat                   VARCHAR(50),
+    code_academie              VARCHAR(50)              
 );
---alter table
+
+
+
 CREATE TABLE _carac_tout_etablissement(
-    effectifs integer,
-    ips real,
-    ecart_type_de_l_ips real,
-    ep VARCHAR(50),
-    uai VARCHAR(50),
-    anne_scolaire VARCHAR
+    effectifs                  integer,
+    ips                        real,
+    ecart_type_de_l_ips        real,
+    ep                         VARCHAR(50),
+    uai                        VARCHAR(50),
+    annee_scolaire              VARCHAR(4)
 
 );
 
 CREATE TABLE _annee(
-    anne_scolaire VARCHAR PRIMARY KEY
+    annee_scolaire              VARCHAR(4) PRIMARY KEY
 );
---alter table
+
+--table association entre etablissement et annee
 CREATE TABLE _carac_college(
     nb_eleves_hors_segpa_hors_ulis integer,
-    nb_eleves_segpa integer,
-    nb_eleves_ulis integer
-    uai VARCHAR(50),
-    anne_scolaire VARCHAR(50)
+    nb_eleves_segpa            integer,
+    nb_eleves_ulis             integer
+    uai                        VARCHAR(50),
+    anne_scolaire              VARCHAR(50)
 );
 
 CREATE TABLE _classe(
-    id_classe VARCHAR(50),
+    id_classe                  VARCHAR(50),
 );
+
 -- alter table
 CREATE TABLE _carac_tout_etablissement(
     nb_eleves_hors_segpa_hors_ulis_selon_niveau integer,
     nb_eleves_segpa_selon_niveau integer,
     nb_eleves_ulis_selon_niveau integer,
-    effectifs_filles integer,
-    effectifs_garcon integer,
-    uai VARCHAR(50),
-    id_classe VARCHAR(50),
-    anne_scolaire VARCHAR(50)
+    effectifs_filles           integer,
+    effectifs_garcon           integer,
+    uai                        VARCHAR(50),
+    id_classe                  VARCHAR(50),
+    anne_scolaire              VARCHAR(50)
 );
 
 CREATE TABLE _a_proximite(
-    uai VARCHAR(50),
-    code_quartier_prioritaire VARCHAR(3)
-
-
+    uai                        VARCHAR(50),
+    code_quartier_prioritaire  VARCHAR(3)
 );
 
+
+--contrainte carac_tout_etablissement
 alter table _carac_tout_etablissement ADD CONSTRAINT competences_fk FOREIGN KEY(lib_competence) REFERENCES _competences(lib_competence);
+
+--contrainte carac_college
+alter table _carac_college ADD CONSTRAINT uai_fk FOREIGN KEY(uai) REFERENCES _etablissement(uai);
+alter table _carac_college ADD CONSTRAINT annee_scolaire_fk FOREIGN KEY(anne_scolaire) REFERENCES _etablissement(uai);
+
 alter table _departement ADD CONSTRAINT fk_departement_region FOREIGN KEY (code_region) REFERENCES _region(code_region);
-alter table _commune ADD CONSTRAINT fk_commune_departement FOREIGN KEY (code_departement) REFERENCES _departement(code_departement);
+alter table _commune ADD CONSTRAINT fk_commune_departement FOREIGN KEY (code_departement) REFERENCES _annee(anne_scolaire);
 
-
-alter table
-alter table
-
-alter table
-alter table
-
+--contrainte de etablissement
+alter table _etablissement ADD CONSTRAINT fk_etablissement_commune FOREIGN KEY (code_commune) REFERENCES _commune(code_insee_de_la_commune);
+alter table _etablissement ADD CONSTRAINT fk_etablissement_quartier FOREIGN KEY (quartier_prioritaire) REFERENCES _quartier_prioritaire(code_quartier_prioritaire);
+alter table _etablissement ADD CONSTRAINT fk_etablissement_type FOREIGN KEY (code_nat) REFERENCES _type(code_nature);
+alter table _etablissement ADD CONSTRAINT fk_etablissement_academie FOREIGN KEY (code_academie) REFERENCES _academie(code_academie);
 alter table
 alter table
 alter table
